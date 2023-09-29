1a9788f66532445333ed2a028b7712d6942857eb
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-09-29 13:09:20 -0500

    initial commit with file structure

---
+++ config/connection.js
@@ -0,0 +1,3 @@
+//1. require mongoose
+//2. create a connection
+//3. export the connection
---
+++ server.js
@@ -0,0 +1,8 @@
+//1. require alldependecies
+  // require mongoose connection
+
+// 2. create the middleware
+  // create port
+  // use express
+
+//3 . start the app
