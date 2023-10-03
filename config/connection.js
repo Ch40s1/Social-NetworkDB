72b845590c3821e0c1432ed999a0ac950917d483
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-10-03 11:50:07 -0500

    added basic server boilerplate

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,6 @@
 //1. require mongoose
+const mongoose = require('mongoose');
 //2. create a connection
+mongoose.connect('mongodb://127.0.0.1:27017/socialNetworkDB')
 //3. export the connection
+module.exports = mongoose.connection;
--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 //1. require alldependecies
-  // require mongoose connection
+const express = require('express');
+ // require mongoose connection
+const db = require('./config/connection');
+// port set
+const PORT = process.env.PORT || 3001;
+// use express
+const app = express();
+
 
 // 2. create the middleware
-  // create port
-  // use express
+  app.use(express.urlencoded({extended: true}));
+  app.use(express.json());
 
 //3 . start the app
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+  })
+})
