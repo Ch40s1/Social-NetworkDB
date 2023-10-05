1af6c80a21c465cc2c81d837ff038b65ead71168
Daniel Z <114247747+Ch40s1@users.noreply.github.com>
2023-10-05 11:43:07 -0500

    can now get user by id and update that user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,44 @@
 const user = require('../models/user')
 
 module.exports = {
-  async getUsers(req, res){
+  async getUsers(req, res) {
     try {
-      const users = await user.find();
+      const users = await user
+        .find()
+        // .select('username email -_id');
+
       res.json(users);
     } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  async createUser(req, res) {
+    try {
+      const dbUserData = await user.create(req.body);
+      res.json(dbUserData);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  },
+  async getSingleUser(req, res) {
+    try {
+      const userData = await user.findOne({ _id: req.params.userId })
+
+      if (!userData) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(userData);
+    } catch (err) {
       res.status(500).json(err);
+    }
+  },
+  async updateUser (req, res){
+    try {
+      const dbUserData = await user.findOneAndUpdate({_id: req.params.userId}, req.body, { new: true });
+      res.json(dbUserData);
+    } catch (error) {
+      res.status(500).json(error);
     }
   }
 };
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const {
   getUsers,
+  createUser,
+  updateUser,
+  getSingleUser
 } = require('../../controllers/userController');
 
-router.route('/').get(getUsers);
+router.route('/').get(getUsers).post(createUser);
+router.route('/:userId').get(getSingleUser).put(updateUser);
 
 module.exports = router;
